206df9eb3cbd0f3c68bbe889a6ea0e1b8caad396
utkucatal <utku_catal@hotmail.com>
2026-03-01 00:13:20 +0100

    feat: add even item multiplier modifier

--- promotions-engine/tests/unit/PriceModifiersTest.php
+++ promotions-engine/tests/unit/PriceModifiersTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\unit;
 use App\DTO\LowestPriceEnquiry;
 use App\Entity\Promotion;
 use App\Filter\Modifier\DateRangeMultiplier;
+use App\Filter\Modifier\EvenItemsMultiplier;
 use App\Filter\Modifier\FixedPriceVoucher;
 use App\Tests\ServiceTestCase;
 use PHPUnit\Framework\Attributes\Test;
@@ -59,4 +60,45 @@ class PriceModifiersTest extends ServiceTestCase
         $this->assertEquals(500, $modifiedPrice);
 
     }
+
+    #[Test]
+    public function EvenItemsMultiplierReturnsACorrectlyModifiedPrice(): void
+    {
+        //Given
+        $enquiry = new LowestPriceEnquiry();
+        $enquiry->setQuantity(5);
+
+        $promotion = new Promotion();
+        $promotion->setName('Buy one get one free');
+        $promotion->setAdjustment(0.5);
+        $promotion->setCriteria(["minimum_quantity" => 2]);
+        $promotion->setType('even_items_multiplier');
+
+        $evenItemsMultiplier = new EvenItemsMultiplier();
+        //When
+        $modifiedPrice = $evenItemsMultiplier->modify(100, 5, $promotion, $enquiry);
+        //Then
+        $this->assertSame(300, $modifiedPrice);
+    }
+
+    #[Test]
+    public function EvenItemsMultiplierCorrectlyCalculatesAlternatives(): void
+    {
+        //Given
+        $enquiry = new LowestPriceEnquiry();
+        $enquiry->setQuantity(5);
+
+        $promotion = new Promotion();
+        $promotion->setName('Buy one get one half price');
+        $promotion->setAdjustment(0.75);
+        $promotion->setCriteria(["minimum_quantity" => 2]);
+        $promotion->setType('even_items_multiplier');
+
+        $evenItemsMultiplier = new EvenItemsMultiplier();
+        //When
+        $modifiedPrice = $evenItemsMultiplier->modify(100, 5, $promotion, $enquiry);
+        //Then
+        $this->assertSame(400, $modifiedPrice);
+    }
+
 }
