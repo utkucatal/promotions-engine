b241de4c514890ca687e05f923298a3d14fdffe7
utkucatal <utku_catal@hotmail.com>
2026-03-01 21:17:57 +0100

    feat: add PriceEnquiryInterface and PriceFilterInterface

---
+++ promotions-engine/src/Filter/PriceFilterInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filter;
+
+use App\Entity\Promotion;
+use App\DTO\PriceEnquiryInterface;
+
+interface PriceFilterInterface extends PromotionsFilterInterface
+{
+    public function apply(PriceEnquiryInterface $enquiry, Promotion ...$promotion): PriceEnquiryInterface;
+}
