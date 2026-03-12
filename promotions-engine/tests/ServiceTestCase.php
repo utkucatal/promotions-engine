6ac405dfc4bafdd839aa23ad56b3b04f3453711e
utkucatal <utku_catal@hotmail.com>
2026-03-12 08:28:06 +0100

    feat: edited for using client in tests

--- promotions-engine/tests/ServiceTestCase.php
+++ promotions-engine/tests/ServiceTestCase.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests;
 
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -9,10 +10,12 @@ use Symfony\Component\Serializer\SerializerInterface;
 class ServiceTestCase extends WebTestCase
 {
     protected ContainerInterface $container;
+    protected KernelBrowser $client;
 
     protected function setUp(): void
     {
         parent::setUp();
-        $this->container = static::createClient()->getContainer();
+        $this->client = static::createClient();
+        $this->container = $this->client->getContainer();
     }
 }
