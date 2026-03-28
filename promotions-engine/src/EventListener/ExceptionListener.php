96bc0736e93cf2f2bf98212321562f3cbf614616
utkucatal <utku_catal@hotmail.com>
2026-03-28 09:05:09 +0100

    feat: add structured error logging to ExceptionListener

--- promotions-engine/src/EventListener/ExceptionListener.php
+++ promotions-engine/src/EventListener/ExceptionListener.php
@@ -3,6 +3,7 @@
 namespace App\EventListener;
 
 use App\Service\ServiceExceptionData;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
@@ -10,15 +11,31 @@ use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class ExceptionListener
 {
+    public function __construct(private LoggerInterface $logger)
+    {
+    }
+
     public function onKernelException(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
 
-        if ($exception instanceof HttpExceptionInterface){
+        if ($exception instanceof HttpExceptionInterface) {
+
             $exceptionData = $exception->getExceptionData();
-        }else{
-            $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+            $this->logger->warning('HTTP exception occurred', [
+                'type'    => $exceptionData->getType(),
+                'status'  => $exceptionData->getStatusCode(),
+                'message' => $exception->getMessage(),
+            ]);
+        } else {
+            $statusCode  = Response::HTTP_INTERNAL_SERVER_ERROR;
             $exceptionData = new ServiceExceptionData($statusCode, $exception->getMessage());
+
+            $this->logger->error('Unhandled exception', [
+                'message' => $exception->getMessage(),
+                'file'    => $exception->getFile(),
+                'line'    => $exception->getLine(),
+            ]);
         }
 
         $response = new JsonResponse($exceptionData->toArray());
